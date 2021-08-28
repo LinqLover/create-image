5b95b4be03bdb233475a6f3525fc0f3c36070c8d
Christoph Thiede <38782922+LinqLover@users.noreply.github.com>
2021-08-28 23:59:20 +0200

    Bootstrapping (#1)
    Script files have been extracted from https://github.com/hpi-swa-lab/squeak-inbox-talk/tree/46eb2611cd8946168b0480a9a677d2cb0b4b902c/scripts. See also: hpi-swa-lab/squeak-inbox-talk#57

---
+++ create_image.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -e
+
+# Read & validate inputs
+if [[ "$PREPARE_SCRIPT" ]]; then
+    prepareScript="$(realpath "$PREPARE_SCRIPT")"
+fi
+if [[ "$POSTPARE_SCRIPT" ]]; then
+    postpareScript="$(realpath "$POSTPARE_SCRIPT")"
+fi
+
+script_dir="$(realpath "$(dirname "$0")")"
+cd "$script_dir"
+fileinScript="$(realpath "prepareImage.st")"
+mkdir output && cd output
+
+# Download and extract latest Trunk release
+files_server="http://files.squeak.org/trunk"
+files_index="files.html"
+wget -O "$files_index" "$files_server"
+build="$(grep -P -o '(?<=a href=")Squeak[^<>]*?-64bit(?=\/")' "$files_index" | tail -1)"
+buildAio="$build-All-in-One.zip"
+wget "$files_server/$build/$buildAio"
+unzip \
+    -d allInOne/ "$buildAio" \
+    -x '*.mo'  # skip superfluous localization files (optimization)
+echo ::set-output name=bundle-path::"$(realpath "$buildAio")"
+
+# Prepare VM execution
+cd allInOne
+# Make squeak.sh capable of passing arguments to the image
+# HACK: Modify squeak.sh because arguments are currently not available for the All-in-One bundles
+# See: https://github.com/squeak-smalltalk/squeak-app/pull/17#issuecomment-876753284
+sed -i '$s/$/ "$@"/' squeak.sh
+if [[ "$CI" == true ]]; then
+    # Add -headless flag to the VM configuration
+    # shellcheck disable=SC2016
+    sed -i 's/\(exec "${VM}"\)\( "${IMAGE}"\)/\1 -headless\2/' squeak.sh
+fi
+
+# Prepare image
+./squeak.sh "$fileinScript" "$prepareScript" "$postpareScript"
+
+# Clean up caches
+shopt -s globstar
+rm -rf ./**/{{package,github}-cache/,\#tmp\#*}
+
+# Write changes back to zip
+zip \
+    -u -r \
+    "../$buildAio" . \
+    -x squeak.sh  # See HACK above
