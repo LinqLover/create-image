48fd60c63578f64c71dfa26415d81f250a3e7ffc
Christoph Thiede <38782922+LinqLover@users.noreply.github.com>
2021-09-03 22:00:07 +0200

    Add tests (#4)
    This adds acceptance tests via GitHub actions for our action.

---
+++ test/extract.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+output="output"
+
+error() {
+    >&2 echo "$@"
+    exit 1
+}
+
+test -n "$1" || error "invalid archive file"
+
+extracted="$output/image"
+mkdir -p "$extracted"
+unzip "$1" -d "$extracted"
+
+ls "$extracted"
+test -f "$extracted/squeak.bat"
+test -f "$extracted/squeak.sh"
+
+echo "extracted image to $extracted"
