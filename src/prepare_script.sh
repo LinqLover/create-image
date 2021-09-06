e3b50600da11a8e5d4c656dbb7a07e525cbc2237
Christoph Thiede <38782922+LinqLover@users.noreply.github.com>
2021-09-06 16:41:40 +0200

    Workaround: Port missing `squeak.sh` features from linux-only variant (#6)
    See https://github.com/squeak-smalltalk/squeak-app/pull/17#issuecomment-876753284. Sound is required for sonyx CD.

--- src/prepare_script.sh
+++ src/prepare_script.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-# HACK: Modify squeak.sh because arguments are currently not available for the All-in-One bundles
+# HACK: Modify squeak.sh because it currently misses several features that are not available for the All-in-One bundles.
 # See: https://github.com/squeak-smalltalk/squeak-app/pull/17#issuecomment-876753284
-sed -i '$s/$/ "$@"/' "$1"
-
-if [[ "$CI" == true ]]; then
-    # Add -headless flag to the VM configuration
-    # shellcheck disable=SC2016
-    sed -i 's/\(exec "${VM}"\)\( "${IMAGE}"\)/\1 -headless\2/' "$1"
-fi
+# Ported features:
+# - ${VMOPTIONS}
+# - detect_sound()
+# - "$@"
+# shellcheck disable=SC1004
+# shellcheck disable=SC2016
+sed -i 's!\(exec\)\( "${VM}"\)\( "${IMAGE}"\)!\
+detect_sound() {\
+    if pulseaudio --check 2>/dev/null ; then\
+        if "${VM}" --help 2>/dev/null | grep -q vm-sound-pulse ; then\
+	    VMOPTIONS="${VMOPTIONS} -vm-sound-pulse"\
+        else\
+            VMOPTIONS="${VMOPTIONS} -vm-sound-oss"\
+            if padsp true 2>/dev/null; then\
+                SOUNDSERVER=padsp\
+            fi\
+        fi\
+    fi\
+}\
+detect_sound\
+\
+\1 ${SOUNDSERVER}\2 ${VMOPTIONS}\3 "$@"!' "$1"
