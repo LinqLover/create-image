ad02b19d793412c521d7924a1c1d952f65f85669
Christoph Thiede <christoph.thiede@student.hpi.de>
2022-03-02 19:25:59 +0100

    Add local test script

---
+++ test/test_local.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Rough local equivalent of .github/workflows/test.yml
+# 🤞 Crossing fingers that this script will not become out of sync with the workflow file
+set -e
+
+output=$(PREPARE_SCRIPT=test/prepare.st POSTPARE_SCRIPT=test/postpare.st src/create_image.sh | tee /dev/tty)
+bundlePath=$(echo "$output" | sed -n 's/.*::set-output name=bundle-path::\(.*\)/\1/p')
+test/extract.sh "$bundlePath"
+output/image/squeak.sh "$(realpath test/test.st)"
