5b1651accbe1db722563393d9395291b41f42fe8
Christoph Thiede <christoph.thiede@student.hpi.de>
2022-04-26 23:07:21 +0200

    Improve robustness of bash scripts

--- test/test_local.sh
+++ test/test_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Rough local equivalent of .github/workflows/test.yml
 # 🤞 Crossing fingers that this script will not become out of sync with the workflow file
-set -e
+set -eo pipefail
 
 output=$(PREPARE_SCRIPT=test/prepare.st POSTPARE_SCRIPT=test/postpare.st src/create_image.sh | tee /dev/tty)
 bundlePath=$(echo "$output" | sed -n 's/.*::set-output name=bundle-path::\(.*\)/\1/p')
